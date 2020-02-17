0a0df0c422c31a5a749ea6f8088efa9b31ea693c
MooseTheGoose <moosa.7@buckeyemail.osu.edu>
2020-02-16 21:38:30 -0500

    Implemented Fireball effect and sprite to the best of my ability

---
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/Effects/BoomerangEffect.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sprint02
+{
+    public class BoomerangEffect : IEffect
+    {
+        private Vector2 position;
+        private readonly Vector2 velocity;
+        private ISprite boomerangSprite;
+        int vertDirection;
+
+        public const int UpDirection = -1;
+        public const int ForwardDirection = 0;
+        public const int DownDirection = 1;
+
+        public BoomerangEffect(Vector2 initialPos)
+        {
+            position = initialPos;
+           /* boomerangSprite = new FireballSprite(initialPos); */
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
---
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/Effects/FireballEffect.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint02
+{
+    public class FireballEffect : IEffect
+    {
+        private Vector2 position;
+        private readonly Vector2 velocity;
+        private ISprite fireballSprite;
+        int vertDirection;
+
+        public const int UpDirection = -1;
+        public const int ForwardDirection = 0;
+        public const int DownDirection = 1;
+
+        public FireballEffect(SpriteBatch batch, Texture2D texture, Vector2 initialPos) 
+        {
+            position = initialPos;
+            fireballSprite = new FireballSprite(texture, batch, initialPos);
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
