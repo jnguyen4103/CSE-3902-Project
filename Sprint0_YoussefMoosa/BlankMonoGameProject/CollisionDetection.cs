01a5c51639f6de2256fe84110b1e585e41fecba0
negretenico <negretenico0@gmail.com>
2020-02-26 14:40:51 -0500

    So I added the CollisionDetection class and the methods I think it'll need. THE ONLY THING IS IM NOT SURE HOW TO KEEP TRACK OF THE EFFECTS LOCATION. Also I added the MouseController because we'll need that too.

---
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/CollisionDetection.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint03
+{
+    class CollisionDetection
+    {
+        Link link;
+        NPC[] enemies;
+        ItemFactory[] items;
+        IEffect[] effects;
+        Rectangle linkRect;
+        Rectangle enemyRect;
+        Rectangle itemRect;
+        Rectangle effectRect;
+        
+        public CollisionDetection(NPC[] Enemies,Link Link, ItemFactory[] Items,IEffect[] Effects)
+        {
+            enemies = Enemies;
+            link = Link;
+            items = Items;
+            effects = Effects;
+        }
+
+        public bool mosterCollisionDetection()
+        {
+            /*Go over the  list of enemies */
+            bool collisionFound = false;
+
+            linkRect = new Rectangle((int)link.Sprite.Position.X, (int)link.Sprite.Position.Y, (int)link.Sprite.GetSize.X, (int)link.Sprite.GetSize.Y);
+
+            for(int i =0; i< enemies.Length; i++)
+            {
+
+                /*Create Rectangel for the current enemey */
+                enemyRect = new Rectangle((int)enemies[i].Sprite.Position.X, (int)enemies[i].Sprite.Position.Y, (int)enemies[i].Sprite.GetSize.X, (int)enemies[i].Sprite.GetSize.Y);
+                
+
+                /*Go over the list of items*/
+
+                for (int j = 0; j< effects.Length; j++ )
+                {
+
+                    /*Create Rectangle for effect*/
+                    effectRect = new Rectangle(effects[j].);
+                    if (enemyRect.Intersects(itemRect))
+                    {
+                        collisionFound = true;
+                    }
+                }
+
+                if (enemyRect.Intersects(linkRect))
+                {
+                    collisionFound = true;
+                }
+
+            }
+            return collisionFound;
+        }
+
+        public void linkCollisionDetection()
+        {
+
+        }
+        
+        public void itemCollisionDetection()
+        {
+
+        }
+    }
+}
---
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/Controller/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint03
+{
+    class MouseController : IController
+    {
+
+        public MouseController()
+        {
+            /*
+             * Needs done
+             */
+        }
+        public void Update()
+        {
+            /*
+             * Needs Done
+             */
+        }
+    }
+}
