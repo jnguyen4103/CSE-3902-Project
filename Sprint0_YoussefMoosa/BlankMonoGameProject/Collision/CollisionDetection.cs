afdb6e9776287b2982cbce66372a325ff582c76f
negretenico <negretenico0@gmail.com>
2020-02-29 18:31:28 -0500

    Fixed Corners

--- Sprint0_YoussefMoosa/BlankMonoGameProject/Collision/CollisionDetection.cs
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/Collision/CollisionDetection.cs
@@ -36,41 +36,57 @@ namespace Sprint03
 
             Console.WriteLine("X: "+Collision.Width+" "+ "Y: "+Collision.Height);
             //Our intersection happened on the left or the right
-            if(Collision.Width<Collision.Height)
+            if (Collision.Height == 0)
             {
-                //Collision happend on the Left
 
-
-
-                if (Receiver.GetPosition.X<Collision.Right && Math.Round( Receiver.GetPosition.X) == Math.Round(Collision.Left))
+                if (Receiver.GetPosition.X < Collision.Right && Math.Round(Receiver.GetPosition.X) == Math.Round(Collision.Left))
                 {
                     returnVal = 2;
                 }
                 //Collision happened on the right 
-                else if(Receiver.GetPosition.X + Receiver.GetSize.X > Collision.Left && Math.Round(Receiver.GetPosition.X + Receiver.GetSize.X) == Math.Round(Collision.Right) )
+                else if (Receiver.GetPosition.X + Receiver.GetSize.X > Collision.Left && Math.Round(Receiver.GetPosition.X + Receiver.GetSize.X) == Math.Round(Collision.Right))
                 {
                     returnVal = 3;
                 }
             }
-            //Our intersection happend on the top or the bottom 
-            else if(Collision.Height<Collision.Width)
+            else
             {
-                //Collision happend on the Top
-
-                Console.WriteLine("TOP: " + Collision.Top);
-                Console.WriteLine("BOTTOM: " + Collision.Bottom);
-                Console.WriteLine("Top OF SPRITE: " + Receiver.GetPosition.Y);
-                Console.WriteLine("Bottom OF SPRITE: " + (Receiver.GetPosition.Y + Receiver.GetSize.Y));
-                if (Receiver.GetPosition.Y == Collision.Top && Receiver.GetPosition.Y > Collision.Bottom)
+                if (Collision.Width < Collision.Height)
                 {
-                    returnVal = 0;
-                }
-                //Collision Happened on the bottom
-                else if(Math.Round(Receiver.GetPosition.Y +Receiver.GetSize.Y) == Math.Round(Collision.Bottom) && Receiver.GetPosition.Y + Receiver.GetSize.Y > Collision.Top)
-                {
-                    returnVal = 1;
-                }
+                    //Collision happend on the Left
 
+
+
+                    if (Receiver.GetPosition.X < Collision.Right && Math.Round(Receiver.GetPosition.X) == Math.Round(Collision.Left))
+                    {
+                        returnVal = 2;
+                    }
+                    //Collision happened on the right 
+                    else if (Receiver.GetPosition.X + Receiver.GetSize.X > Collision.Left && Math.Round(Receiver.GetPosition.X + Receiver.GetSize.X) == Math.Round(Collision.Right))
+                    {
+                        returnVal = 3;
+                    }
+                }
+                //Our intersection happend on the top or the bottom 
+                else if (Collision.Height < Collision.Width)
+                {
+                    //Collision happend on the Top
+
+                    Console.WriteLine("TOP: " + Collision.Top);
+                    Console.WriteLine("BOTTOM: " + Collision.Bottom);
+                    Console.WriteLine("Top OF SPRITE: " + Receiver.GetPosition.Y);
+                    Console.WriteLine("Bottom OF SPRITE: " + (Receiver.GetPosition.Y + Receiver.GetSize.Y));
+                    if (Receiver.GetPosition.Y == Collision.Top && Receiver.GetPosition.Y > Collision.Bottom)
+                    {
+                        returnVal = 0;
+                    }
+                    //Collision Happened on the bottom
+                    else if (Math.Round(Receiver.GetPosition.Y + Receiver.GetSize.Y) == Math.Round(Collision.Bottom) && Receiver.GetPosition.Y + Receiver.GetSize.Y > Collision.Top)
+                    {
+                        returnVal = 1;
+                    }
+
+                }
             }
             Console.WriteLine(returnVal);
             return returnVal;
