3fd8e334d4926a060c8840b3f0a1a933b2cd12d3
negretenico <negretenico0@gmail.com>
2020-03-01 01:06:05 -0500

    Implemented Tile

---
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/GameObjects/Tile/TileSprite.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Sprint03
+{
+    class TileSprite : Sprite
+    {
+
+        public TileSprite(Game1 game, String name, Texture2D texture, Vector2 spawn, SpriteBatch batch)
+        {
+            this.Game = game;
+            this.Batch = batch;
+            this.Name = name;
+            this.Size = new Vector2(16, 16);
+            this.Position = spawn;
+            this.Texture = texture;
+            this.ChangeSpriteAnimation(name);
+        }
+
+ 
+
+        public override void ChangeSpriteAnimation(string newSpriteName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
