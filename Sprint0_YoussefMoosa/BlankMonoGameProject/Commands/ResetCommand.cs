de72fc16cc53c2abaeb18e9b5ac40de8058d235d
Stephen Hogg <hogg.36@buckeyemail.osu.edu>
2020-03-04 17:48:57 -0500

    Fixed reset and Stalfos bugs

--- Sprint0_YoussefMoosa/BlankMonoGameProject/Commands/ResetCommand.cs
+++ Sprint0_YoussefMoosa/BlankMonoGameProject/Commands/ResetCommand.cs
@@ -13,8 +13,10 @@
         {
             Game.Link.SpriteLink.Position = Game.LinkSpawn;
             Game.Link.StateMachine.DownState();
+            Game.Link.StateMachine.IdleState();
             Game.Link = new Link(Game.SpriteLink, Game);
             Game.Link.HP = Game.Link.MaxHP;
+            Game.SpriteLink.BaseSpeed = 1f;
             Game.CurrentRoom.UnloadRoom();
             Game.RFactory.ResetRooms();
             Game.RFactory.LoadRoom("Room0");
