8fbf78ee1944e09263768ecefb1ee4668f9276a6
Xueying Liang <xueying.liang1999@gmail.com>
2020-04-12 22:42:49 -0400

    Implemented part of Item Selection

---
+++ Legend of Zelda/BlankMonoGameProject/Commands/Item Selection/MenuSelectionRight.cs
@@ -0,0 +1,27 @@
+﻿/* Contributors:
+ * Xueying Liang
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint03
+{
+    internal class MenuSelectionRight : ICommand
+    {
+        private readonly ISelectionMenu Menu;
+
+        public MenuSelectionRight(ISelectionMenu menu)
+        {
+            Menu = menu;
+        }
+
+        public void Execute()
+        {
+            Menu.SelectionRight();
+        }
+    }
+}
