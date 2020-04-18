a777f2573130c0d70967e68ce35429a05be0a513
Grant Gabel <gabel.170@buckeyemail.osu.edu>
2020-04-18 15:25:10 -0400

    added bare bones skeleton for game state interface

---
+++ Legend of Zelda/BlankMonoGameProject/GameState/GamePlayingState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankMonoGameProject.GameState
+{
+	class GamePlayingState : IGameState
+	{
+		public void Draw()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Legend of Zelda/BlankMonoGameProject/GameState/IGameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankMonoGameProject.GameState
+{
+	public interface IGameState
+	{
+		void Update();
+		void Draw();
+	}
+}
